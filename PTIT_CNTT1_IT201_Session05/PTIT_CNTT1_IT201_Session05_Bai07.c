0d672c60c072fad1d6050ab8b54d9ac657032c45
mtuyen260609 <vuminhtuyen26092006@gmail.com>
2025-06-26 21:15:18 +0700

    Add files via upload

---
+++ PTIT_CNTT1_IT201_Session05/PTIT_CNTT1_IT201_Session05_Bai07.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int fibonaci(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return fibonaci(n-1) + fibonaci(n-2);
+}
+int main() {
+    int n;
+    int result[100];
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n<=0) {
+        printf("Khong hop le");
+    }
+    for (int i = 0; i < n; i++) {
+        result[i] = fibonaci(i);
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", result[i]);
+    }
+    return 0;
+}
