f72cac08cbfcac06c44e233d4b5e514de0b89e7a
mtuyen260609 <vuminhtuyen26092006@gmail.com>
2025-07-10 21:59:47 +0700

    PTIT_CNTT1_IT201_Session15_Bai01.c

--- PTIT_CNTT1_IT201_Session15/PTIT_CNTT1_IT201_Session15_Bai01.c
+++ PTIT_CNTT1_IT201_Session15/PTIT_CNTT1_IT201_Session15_Bai01.c
@@ -4,14 +4,14 @@ struct Queue {
     int *array;
     int front;
     int rear;
-    int maxSize;
+    int capacity;
 };
 struct Queue* createQueue(int size) {
     struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
     q->array = (int*)malloc(size * sizeof(int));
     q->front = 0;
     q->rear = -1;
-    q->maxSize = size;
+    q->capacity = size;
     return q;
 }
 int main() {
